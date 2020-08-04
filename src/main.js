a50695b3cc7ab7f0d1b14dd25106d2cda9f047f6
tanghuang-liu <916650458@qq.com>
2020-08-04 16:33:13 +0800

    test1

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ import store from './store'
 import router from './router'
 
 import './permission' // permission control
-import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
 
